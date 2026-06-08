d4a3d7c73fa82a7edb89db0800fa7dbb74f4a9a1
IT24104152 <gayuthdemika@gmail.com>
2026-06-08 21:01:43 +0530

    feat: embedding functionality - ollama embeddings implemented through and interface

---
+++ internal/embedding/ollama_embedder.go
@@ -0,0 +1,53 @@
+package embedding
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type OllamaEmbedder struct {
+	BaseURL string
+	Model   string
+}
+
+func NewOllamaEmbedder() *OllamaEmbedder {
+	return &OllamaEmbedder{
+		BaseURL: "http://localhost:11434",
+		Model:   "nomic-embed-text", // or whatever you installed
+	}
+}
+
+type ollamaReq struct {
+	Model string `json:"model"`
+	Input string `json:"prompt"`
+}
+
+type ollamaResp struct {
+	Embedding []float32 `json:"embedding"`
+}
+
+func (o *OllamaEmbedder) Embed(text string) ([]float32, error) {
+
+	reqBody := ollamaReq{
+		Model: o.Model,
+		Input: text,
+	}
+
+	b, _ := json.Marshal(reqBody)
+
+	resp, err := http.Post(
+		o.BaseURL+"/api/embeddings",
+		"application/json",
+		bytes.NewBuffer(b),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var out ollamaResp
+	json.NewDecoder(resp.Body).Decode(&out)
+
+	return out.Embedding, nil
+}
